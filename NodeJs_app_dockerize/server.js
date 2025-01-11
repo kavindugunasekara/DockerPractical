b87f285d9152e1c5e2d9cf11de2123e5aaa94462
WAKD Gunasekara <137909922+kavinduGunasekara@users.noreply.github.com>
2025-01-11 17:12:09 +0530

    configure Docker Volumes
    when your application do changes it need to apply on images as well as container then you need to configure .Docker Volumes to real time file changes to apply it running continer with docker volumes

--- NodeJs_app_dockerize/server.js
+++ NodeJs_app_dockerize/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 app.get("/posts", async (req, res) => {
   try {
     const responce = await fetch(
-      "https://jsonplaceholder.typicode.com/posts?_limit=30"
+      "https://jsonplaceholder.typicode.com/posts?_limit=10"
     );
     const data = await responce.json();
     res.json(data);
